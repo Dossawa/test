efcc76611fccdd54bd756b3bf19fec282792d82f
Dossawa <66283098+Dossawa@users.noreply.github.com>
2022-11-09 23:39:39 +0100

    Delete form.js

--- form.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector(".se-faire-rappeler");
-const modalTriggers = document.querySelectorAll(".modal-trigger");
-
-modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
-
-function toggleModal(){
-    form.classList.toggle("active")
-}
-
